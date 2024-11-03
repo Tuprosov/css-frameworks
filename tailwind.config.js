42c2c6d60008bf34552c7675aa77bf9eb67fa53b
Shahzod <shahzodwill@gmail.com>
2024-11-03 18:47:51 +0100

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/**/*.html", "!./node_modules/**/*"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
